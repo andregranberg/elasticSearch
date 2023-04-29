10fce87dcaefc703e682916b434ec06e82144ea5
andregranberg <38399597+andregranberg@users.noreply.github.com>
2023-04-29 03:40:47 +0200

    Update add_article.py

--- add_article.py
+++ add_article.py
@@ -4,8 +4,8 @@ es = Elasticsearch([{"host": "localhost", "port": 9200, "scheme": "http"}])
 index_name = "news_articles"
 
 news_articles = {
-    "title": "added article",
-    "text": "this is an article added with the add_article.py script"
+    "title": input('Enter title: '),
+    "text": input('Enter text: ')
 }
 
 es.index(index=index_name, body=news_articles)
